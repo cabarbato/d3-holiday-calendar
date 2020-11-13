3cefb069a44e248e7dd219ac4f80d63065a2e4fa
Colleen Barbato <cawalizer@gmail.com>
2020-11-13 17:52:20 -0500

    Updates

---
+++ precache-manifest.03ee89b00f19f5f0fc24ceb282bfea76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132b98fda26d69893b34dbed50b74aa9",
+    "url": "/d3-holiday-calendar/index.html"
+  },
+  {
+    "revision": "591cc04cd23e3d1728aa",
+    "url": "/d3-holiday-calendar/static/css/2.da734ad0.chunk.css"
+  },
+  {
+    "revision": "b3e2214fc975a268b506",
+    "url": "/d3-holiday-calendar/static/css/main.4f9a0a0e.chunk.css"
+  },
+  {
+    "revision": "591cc04cd23e3d1728aa",
+    "url": "/d3-holiday-calendar/static/js/2.7ebe15e4.chunk.js"
+  },
+  {
+    "revision": "cfb9baded6e607bb2db5d70c672aedcf",
+    "url": "/d3-holiday-calendar/static/js/2.7ebe15e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e2214fc975a268b506",
+    "url": "/d3-holiday-calendar/static/js/main.113296af.chunk.js"
+  },
+  {
+    "revision": "315e65cf50844e8007dd",
+    "url": "/d3-holiday-calendar/static/js/runtime-main.299b5a00.js"
+  }
+]);
